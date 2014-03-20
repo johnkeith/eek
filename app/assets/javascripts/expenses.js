501591c4ddc7ac54aad2725c43d12bc1b9a94b53
John Keith <johnkeith@outlook.com>
2014-03-20 15:12:46 -0400

    testing expense.js

--- app/assets/javascripts/expenses.js
+++ app/assets/javascripts/expenses.js
@@ -2,8 +2,5 @@
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
 <script>
-  $('#import-button').click(function(e) {
-    e.preventDefault();
-    $('#import-modal').modal();
-  });
+
 </script>
