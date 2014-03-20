d1f36b006a000226d06a73d740ae61338720c442
John Keith <johnkeith@outlook.com>
2014-03-20 18:11:27 -0400

    redirect fom admin settings if not admin

--- app/controllers/admin/configurables_controller.rb
+++ app/controllers/admin/configurables_controller.rb
@@ -1,4 +1,8 @@
 class Admin::ConfigurablesController < ApplicationController
-	include ConfigurableEngine::ConfigurablesController
+	before_filter :authenticate_user!
+  before_filter do
+    redirect_to root_url unless current_user.admin?
+  end
+  include ConfigurableEngine::ConfigurablesController
 	layout 'col-md-8-offset-md-2'
 end
