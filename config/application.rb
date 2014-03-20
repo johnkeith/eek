95e78639d5cf1a2a1f7598e8c337b4c4084f1ecc
John Keith <johnkeith@outlook.com>
2014-03-20 15:37:34 -0400

    removed config line from app.rb

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-config.assets.initialize_on_precompile = false
+
 Bundler.require(:default, Rails.env)
 
 module TrackExpenses
@@ -26,6 +26,6 @@ module TrackExpenses
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
+    
   end
 end
